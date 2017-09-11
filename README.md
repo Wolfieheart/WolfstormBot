b4766661d0db78937511c7a73fb31af4a28a3213
Wolfst0rm <xMakeItRain94@gmail.com>
2017-09-11 10:54:36 +0200

    [DEV 0.1] Connections, Shutdown and Commands
    Built off Jagrosh's Example (See Readme)

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Git Repo
+#WolfstormBot
+[![Build Status](https://travis-ci.org/Wolfst0rm/WolfstormBot.svg?branch=master)](https://travis-ci.org/Wolfst0rm/WolfstormBot) 
+[![Dependency Status](https://www.versioneye.com/user/projects/59b64c85368b08003d0e8bdf/badge.svg?style=flat-square)](https://www.versioneye.com/user/projects/59b64c85368b08003d0e8bdf)
+
+NOTE: Currently under development, therefore NOT a finished product. Will be under the dev hammer for quite a while.
+
+Pull Requests are welcomed. Please report all issues you find through your tests in [Issues](https://github.com/Wolfst0rm/WolfstormBot/issues).
+
+##Dependencies
+* [JDA](https://github.com/DV8FromTheWorld/JDA/) by DV8FromTheWorld and Kantenkugel
+* [JDA-Utilities](https://github.com/JDA-Applications/JDA-Utilities)by Jagrosh
+
+####Referenced from: [ExampleBot](https://github.com/jagrosh/ExampleBot/) by Jagrosh
+
+NOTE: You will need to create your own Config.JSON for this to work. I wont provide mine for security reasons.
+
+## Documentation
+None at this time. As this project expands - there will be some!
+
+## Contributions
+Coming Soon.... 
+
+##Code of Conduct
+Coming Soon....
---
+++ src/main/java/com/Wolfstorm/bot/Bot.java
@@ -0,0 +1,83 @@
+package com.Wolfstorm.bot;
+
+import com.jagrosh.jdautilities.commandclient.CommandClientBuilder;
+import com.jagrosh.jdautilities.commandclient.examples.AboutCommand;
+import com.jagrosh.jdautilities.commandclient.examples.PingCommand;
+import com.jagrosh.jdautilities.commandclient.examples.ShutdownCommand;
+import com.jagrosh.jdautilities.waiter.EventWaiter;
+import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+
+public class Bot
+{
+    public static void main(String[] args) throws IOException, LoginException, RateLimitedException, IllegalArgumentException {
+
+       //Config.JSON contains two lines
+       List<String> list = Files.readAllLines(Paths.get("config.json"));
+
+       //Get BotToken from Config.JSON
+       String botToken = list.get(0);
+
+       //Get OwnerID from Config.JSON
+       String ownerID = list.get(1);
+
+       //Define EventWaiter - Add this to JDA
+        EventWaiter wait4evt = new EventWaiter();
+
+        //Command Client
+        CommandClientBuilder client = new CommandClientBuilder();
+
+        //Default Command - "Type [p]Help" (NOTE = [p] is Prefix)
+        client.useDefaultGame();
+
+        //Set OwnerID
+        client.setOwnerId(ownerID);
+
+        //Emojis to indicate Success, Warning and Failure
+        client.setEmojis("\uD83C\uDF96","⚠️", "✖️");
+
+        //set Prefix
+        client.setPrefix("+");
+
+        //add Commands
+        client.addCommands(
+                //About Command
+                new AboutCommand(Color.RED, "WolfstormBot 0.1-dev",
+                        new String[]{"Commands", "Examples", "and more..."},
+                        new Permission[]{Permission.ADMINISTRATOR}),
+
+                //Ping Command
+                new PingCommand(),
+
+                //Shutdown Command
+                new ShutdownCommand());
+
+        //Start The Bot
+        new JDABuilder(AccountType.BOT)
+                //Set Not Token
+                .setToken(botToken)
+
+                //Set Status while Bot is Loading
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setGame(Game.of("LOADING....."))
+
+                //Add All Listeners here
+                .addEventListener(wait4evt)
+                .addEventListener(client.build())
+
+                //Start Up HYPE!
+                .buildAsync();
+    }
+}
+
